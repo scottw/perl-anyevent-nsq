98c444c07df3cbb86c7b303877840fb621a0fb87
David Oswald <david.oswald@endurance.com>
2015-11-25 11:50:38 -0700

    Added ExtUtils::MakeMaker based distribution files.

--- lib/AnyEvent/NSQ.pm
+++ lib/AnyEvent/NSQ.pm
@@ -4,6 +4,11 @@ package AnyEvent::NSQ;
 # VERSION
 # AUTHORITY
 
+# The version dance.  Underscores indicate dev version.
+our $VERSION=0.01_001;
+$VERSION = eval $VERSION;
+
+
 use strict;
 use warnings;
 
